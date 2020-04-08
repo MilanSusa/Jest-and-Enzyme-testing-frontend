399c4b2231aebb553d2fc6ddf2773b8bfa1dc1fe
Milan <milan_susa@hotmail.com>
2020-04-08 18:21:26 +0200

    Add Navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const Navigation = props => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="/inferences">Inferences</Nav.Link>
+                <Nav.Link href="/persons">Persons</Nav.Link>
+                <Nav.Link href="/models">Models</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default Navigation;
