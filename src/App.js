4c128ef7a896e3e08165b3a5972ef80bebbddbfb
Milan <milan_susa@hotmail.com>
2020-04-08 18:39:13 +0200

    Modify App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Redirect, Route, Switch} from "react-router";
+import Layout from "./components/Layout/Layout";
+import Home from "./components/Home/Home";
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Redirect to="/" />
+                </Switch>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
