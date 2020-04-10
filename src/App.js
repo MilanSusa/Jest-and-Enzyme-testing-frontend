dfe9a43658f0fa47d626b823478d334821465fe5
Milan <milan_susa@hotmail.com>
2020-04-10 22:01:30 +0200

    Add /inferences route

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {Redirect, Route, Switch} from "react-router";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
+import ComposedInferences from "./components/ComposedInferences/ComposedInferences";
 
 const App = () => {
     return (
         <div className="App">
             <Layout>
                 <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Redirect to="/" />
+                    <Route path="/inferences" render={props => <ComposedInferences {...props} />}/>
+                    <Route path="/" exact component={Home}/>
+                    <Redirect to="/"/>
                 </Switch>
             </Layout>
         </div>
