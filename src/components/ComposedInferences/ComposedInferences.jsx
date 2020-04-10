a7052bc4fd8d6575f299b05963f9a4c591d51023
Milan <milan_susa@hotmail.com>
2020-04-10 21:58:09 +0200

    Add axios and ComposedInferences component

---
+++ src/components/ComposedInferences/ComposedInferences.jsx
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from "react";
+import ComposedInference from "./ComposedInference/ComposedInference";
+import axios from 'axios';
+import Container from "react-bootstrap/Container";
+
+const ComposedInferences = props => {
+    const [inferences, setInferences] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await axios.get("http://localhost:8080/api/v1/inferences");
+            const data = res.data.data;
+            setInferences(data);
+        })();
+    }, []);
+
+    return (
+        <Container>
+            {inferences.map(inference => <ComposedInference key={inference.id} data={inference}/>)}
+        </Container>
+    );
+};
+
+export default ComposedInferences;
