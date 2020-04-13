3ea27074486dd46beb68d954e076b5ac1d5c2bf7
Milan <milan_susa@hotmail.com>
2020-04-13 18:44:34 +0200

    Add tests for Payload component

---
+++ src/components/Payloads/Payload/Payload.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Payload from "./Payload";
+import PayloadTypeRenderer from "../../PayloadTypeRenderer/PayloadTypeRenderer";
+
+describe('<Payload />', () => {
+    let wrapper = null;
+
+    beforeEach(() => {
+        wrapper = shallow(<Payload data={{}}/>);
+    });
+
+    it('should render PayloadTypeRenderer with content as props', () => {
+        wrapper.setProps({
+            data: {
+                content: "dummy"
+            }
+        });
+        expect(wrapper.contains(<PayloadTypeRenderer data={{content: "dummy"}}/>)).toEqual(true);
+    });
+
+    it('should render PayloadTypeRenderer sub component with url as props', () => {
+        wrapper.setProps({
+            data: {
+                url: "dummy"
+            }
+        });
+        expect(wrapper.contains(<PayloadTypeRenderer data={{url: "dummy"}}/>)).toEqual(true);
+    });
+
+    it('should render PayloadTypeRenderer with empty object as props', () => {
+        expect(wrapper.contains(<PayloadTypeRenderer data={{}}/>)).toEqual(true);
+    });
+});
